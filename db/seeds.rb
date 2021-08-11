a858cf39750470c5b47a0646f304ed94e71ff74f
Janus Vollmer <janus.vollmer@gmail.com>
2021-08-11 13:52:53 +0200

    implemented the routes, controller, controller actions and views for the restau rant model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+columbia = { name: "Columbia", address: "Am alten Markt", category: 'italian', phone_number: '+490800555777333' }
+lingling = { name: "Ling Ling", address: "Pagodenstr. 5", category: 'chinese', phone_number: '+490809754335679' }
+sushi_sano = { name: "Sushi Sano", address: "Munich", category: 'japanese', phone_number: '+4908753332000012' }
+ameuse_gueule = { name: "Ameuse Gueule", address: "Rue de Palais", category: 'french', phone_number: '+49080986666432' }
+frittenwirt = { name: "Frittenwirt", address: "An der Friteuse", category: 'belgian', phone_number: '+4908066664442009' }
+
+[columbia, lingling, sushi_sano, ameuse_gueule, frittenwirt].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts 'Finished'
